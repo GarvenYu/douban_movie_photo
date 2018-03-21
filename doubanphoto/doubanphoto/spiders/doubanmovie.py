794b3103baf65fa9d8a870169f9221e2c0088896
ykbboom <1747603726@qq.com>
2018-03-21 23:25:11 +0800

    爬取所有页的大图

--- doubanphoto/doubanphoto/spiders/doubanmovie.py
+++ doubanphoto/doubanphoto/spiders/doubanmovie.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy.http import Request, FormRequest
+import re
 
 
 class DoubanmovieSpider(scrapy.Spider):
     name = 'doubanmovie'
     # allowed_domains = ['douban.movie.com']
-    start_urls = ['https://movie.douban.com/subject/1418640/all_photos']
+    start_urls = ['https://movie.douban.com/subject/1418640/photos?type=S']
     login_url = 'https://www.douban.com/accounts/login'
+    photo_per_page = 30
+    next_page_url = "https://movie.douban.com/subject/1418640/photos?" \
+                    "type=S&start=%s&sortby=like&size=a&subtype=a"
 
     def start_requests(self):
         yield Request(self.login_url, callback=self.login)
@@ -21,7 +25,18 @@ class DoubanmovieSpider(scrapy.Spider):
             yield from super().start_requests()
 
     def parse(self, response):
-        for div in response.css('div.mod:nth-child(1) ul li'):
+        page_url_list = response.css('div.paginator>a::attr(href)').extract()
+        page_start_re = re.compile(r'start=(\d+)')
+        page_start_end = page_start_re.findall(page_url_list[len(page_url_list) - 1])[0]  # 最后一页的起始start
+        page = int(page_start_end) // self.photo_per_page  # 页数
+        for i in range(0, page+1):  # 要包含最后一页
+            if i == 0:
+                yield scrapy.Request(self.start_urls[0], callback=self.parse_test)
+            else:
+                yield scrapy.Request(self.next_page_url % (self.photo_per_page*i), callback=self.parse_test)
+
+    def parse_test(self, response):
+        for div in response.css('ul.clearfix li'):
             link = div.css('a::attr(href)').extract_first()
             yield scrapy.Request(link, callback=self.download_images)
 
